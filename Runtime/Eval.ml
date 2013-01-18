59b636112e9c33f4764a7b2aa5a2744090b01343
Nagadum <dnjata@gmail.com>
2013-01-18 14:55:34 +0100

    Evaluation of %, &&, || and not.

--- Runtime/Eval.ml
+++ Runtime/Eval.ml
@@ -4,14 +4,18 @@ open TypeEnv
 
 let rec eval_expr e env =
   match e.edesc with
-    | Call(e1, "add", e2::_) ->  begin match ((eval_expr e1 env), (eval_expr e2 env)) with
-	| (Int a, Int b) -> Int(a+b)
+    | Call(e1, "not", _) ->  begin match (eval_expr e1 env) with
+	| Boolean a -> Boolean(not a)
 	| _ -> Null
     end
     | Call(e1, "sub", e2::_) ->  begin match ((eval_expr e1 env), (eval_expr e2 env)) with
 	| (Int a, Int b) -> Int(a-b)
 	| _ -> Null
-      end
+    end
+    | Call(e1, "add", e2::_) ->  begin match ((eval_expr e1 env), (eval_expr e2 env)) with
+	| (Int a, Int b) -> Int(a+b)
+	| _ -> Null
+    end
     | Call(e1, "mul", e2::_) ->  begin match ((eval_expr e1 env), (eval_expr e2 env)) with
 	| (Int a, Int b) -> Int(a*b)
 	| _ -> Null
@@ -19,7 +23,19 @@ let rec eval_expr e env =
     | Call(e1, "div", e2::_) ->  begin match ((eval_expr e1 env), (eval_expr e2 env)) with
 	| (Int a, Int b) -> Int(a/b)
 	| _ -> Null
-      end
+    end
+    | Call(e1, "mod", e2::_) ->  begin match ((eval_expr e1 env), (eval_expr e2 env)) with
+	| (Int a, Int b) -> Int(a mod b)
+	| _ -> Null
+    end
+    | Call(e1, "and", e2::_) ->  begin match ((eval_expr e1 env), (eval_expr e2 env)) with
+	| (Boolean a, Boolean b) -> Boolean (a && b)
+	| _ -> Null
+    end
+    | Call(e1, "or", e2::_) ->  begin match ((eval_expr e1 env), (eval_expr e2 env)) with
+	| (Boolean a, Boolean b) -> Boolean (a || b)
+	| _ -> Null
+    end
     | Define (varname, vartype, varvalue, e) -> 
 	let newenv = TypeEnv.addVar env varname (eval_expr varvalue env) in
 	  eval_expr e newenv
